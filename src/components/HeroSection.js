e87436706b279a3887f7d151e7af0ccab38c0838
Chloe Lau <chlo@Chloes-MacBook-Pro.local>
2023-03-19 15:16:15 +0000

    make changes

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import '../App.css';
-import { Button } from './Button';
-import './HeroSection.css';
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
-        <div className='hero-btns'>
-            <img className="home-button-background"src='images/coder.jpg'></img>
-            <Button
-                className='btn'
-                buttonStyle='btn--outline'
-                buttonSize='btn--large'
-                destination='dev'
-            >
-                The Coder
-            </Button>
-            <img className="home-button-background" src='images/shoot1.jpg'></img>
-            <Button
-                className='btn'
-                buttonStyle='btn--outline'
-                buttonSize='btn--large'
-                destination='portfolio'
-
-            >
-                The Model 
-            </Button>
-        </div>
+    <div className="hero-container">
+      <div className="hero-btns">
+        <img className="home-button-background" src="images/coder.jpg"></img>
+        <Button
+          className="btn"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+          destination="dev"
+        >
+          The Coder
+        </Button>
+        <img className="home-button-background" src="images/shoot1.jpg"></img>
+        <Button
+          className="btn"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+          destination="portfolio"
+        >
+          LALALA
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
